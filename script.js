aa2934fafbe6ffe272f62f2e4607d76b3ebcdac9
tikosaghinadze <teasaghinadze20@gmail.com>
2024-02-02 23:00:58 +0400

    Started desktop design and implemented dynamic image change for desktop view

---
+++ script.js
@@ -0,0 +1,6 @@
+// Check if the viewport width is greater than or equal to 1440 pixels
+if (window.matchMedia("(min-width: 1440px)").matches) {
+  document.querySelector(".mobile-img").src =
+    "./assets/images/illustration-sign-up-desktop.svg";
+}
+console.log(2);
